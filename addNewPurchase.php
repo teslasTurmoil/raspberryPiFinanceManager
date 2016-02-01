7891d98da00675bb8157f60a92b538ee5160c212
Justin Thaler <jst172@gmail.com>
2016-02-01 12:42:17 -0500

    updating to new fields and to allow for new database format. Purchases can now be split between multiple budgets.

--- addNewPurchase.php
+++ addNewPurchase.php
@@ -32,7 +32,7 @@
         } 
     $conn->close();
 ?>
-            <form action="savePurchase.php" method="post">
+            <form name="mainForm" action="savePurchase.php" onsubmit="return validateForm()" method="post">
                 <label><input type="number" name="numItems" id="numItems" min="0" value="0">
                     How many purchases do you want to enter?
                 </label>
@@ -75,11 +75,15 @@
                                         
                     function addFieldset(i) {
                         var budgets = [];
+                        budgets[0] = document.createElement("option");
+                        budgets[0].text = "";
+                        budgets[1] = document.createElement("option");
+                        budgets[1].text = "SPLIT";
                         var accounts = [];
                         <?php
                             for ($i=0; $i<count($budgets); $i++){
-                                echo "budgets[{$i}] = document.createElement(\"option\");";
-                                echo "budgets[{$i}].text = \"" . $budgets[$i] . "\";\n\t\t\t\t\t\t";
+                                echo "budgets[{$i}+2] = document.createElement(\"option\");";
+                                echo "budgets[{$i}+2].text = \"" . $budgets[$i] . "\";\n\t\t\t\t\t\t";
                             }
                             
                             for ($i=0; $i<count($accounts); $i++){
@@ -103,48 +107,58 @@
                         input[0].setAttribute("required","required");
                         
                         input[1] = document.createElement("input");
-                        input[1].type = "number";
-                        input[1].setAttribute("name","amount" + i);
-                        input[1].setAttribute("min", "0.01");
-                        input[1].setAttribute("step","0.01");
+                        input[1].type = "text";
+                        input[1].setAttribute("name","memo" + i);
+                        input[1].setAttribute("size", "20");
+                        input[1].setAttribute("maxlength","200");
                         input[1].setAttribute("required","required");
                         
                         input[2] = document.createElement("input");
-                        input[2].type = "text";
-                        input[2].setAttribute("name","personName" + i);
-                        input[2].setAttribute("size", "10");
-                        input[2].setAttribute("maxlength","100");
+                        input[2].type = "number";
+                        input[2].setAttribute("name","amount" + i);
+                        input[2].setAttribute("min", "0.01");
+                        input[2].setAttribute("step","0.01");
                         input[2].setAttribute("required","required");
                         
                         input[3] = document.createElement("input");
-                        input[3].type = "date";
-                        input[3].setAttribute("name","date" + i);
+                        input[3].type = "text";
+                        input[3].setAttribute("name","personName" + i);
+                        input[3].setAttribute("size", "10");
+                        input[3].setAttribute("maxlength","100");
                         input[3].setAttribute("required","required");
+                        
+                        input[4] = document.createElement("input");
+                        input[4].type = "date";
+                        input[4].setAttribute("name","date" + i);
+                        input[4].setAttribute("required","required");
                         var d = new Date();
                         //var offset = new Date().getTimezoneOffset();
                         //d.setUTCHours(d.getUTCHours() + offset/60);  //need to set a locale
                         d.setUTCHours(d.getUTCHours() - 5);  //setting locale to US Eastern
-                        input[3].setAttribute("value", d.toISOString().substring(0, 10));
-                        
-                        input[4] = document.createElement("select");
-                        input[4].setAttribute("name","budget" + i);
-                        input[4].setAttribute("required","required");
-                        for(var index2 = 0; index2< budgets.length; index2++){
-                            input[4].add(budgets[index2]);
-                        }
+                        input[4].setAttribute("value", d.toISOString().substring(0, 10));
                         
                         input[5] = document.createElement("select");
-                        //input[5].type = "text";
-                        input[5].setAttribute("name","account" + i);
+                        input[5].setAttribute("id", "budget" + i);
+                        input[5].setAttribute("name","budget" + i);
                         input[5].setAttribute("required","required");
+                        for(var index2 = 0; index2< budgets.length; index2++){
+                            input[5].add(budgets[index2]);
+                        }
+                        input[5].onchange = checkBudgetSplit;
+                        
+                        input[6] = document.createElement("select");
+                        //input[5].type = "text";
+                        input[6].setAttribute("name","account" + i);
+                        input[6].setAttribute("required","required");
                         for(var index2 = 0; index2< accounts.length; index2++){
-                            input[5].add(accounts[index2]);
+                            input[6].add(accounts[index2]);
                         }
                         
                         var table = document.createElement("table");
                         table.setAttribute("class", "dataEntry");
+                        table.setAttribute("id", "pTable" + i)
                         var row, cell1, cell2;
-                        var desc = ["Location: ", "Amount: ", "Who: ", "Date: " , "Budget: ", "Account: "];
+                        var desc = ["Location: ", "Memo: ", "Amount: ", "Who: ", "Date: " , "Budget: ", "Account: "];
                         for(var index2 = 0; index2 <input.length; index2++){
                             row = table.insertRow(index2);
                             cell1 = row.insertCell(0);
@@ -156,12 +170,150 @@
                         }   
                         fieldset.appendChild(table); 
                         
+                        //create divider for handling splitting purchases between multiple budgets
+                        var div = document.createElement("div");
+                        div.setAttribute("id", "purchaseSplit" + i);
+                        div.setAttribute("name", "purchaseSplit" + i);
+                        div.hidden = true;
                         document.getElementById("purchases").appendChild(fieldset);
-                        
+                        fieldset.appendChild(div);
+                                                
                     }
                     function deleteFieldset(i) {
                         var id = "#purchase" + i;
                         $(id).remove();
+                    }
+                    
+                    function checkBudgetSplit(){
+                        var i = this.id.slice(-1);
+                        var table = document.getElementById("pTable"+i);
+                        if (this.value === "SPLIT"){
+                            var splitCounter = document.createElement("input");
+                            splitCounter.type = "number";
+                            splitCounter.setAttribute("id","splitCounter" + i);
+                            splitCounter.setAttribute("name","splitCounter" + i);
+                            splitCounter.setAttribute("min", "2");
+                            splitCounter.setAttribute("step","1");
+                            splitCounter.setAttribute("required","required");
+                            splitCounter.onchange = splitInputChange;
+                            splitCounter.dataset.lastValue = 0;
+                            
+                            var row, cell1, cell2;
+                            row = table.insertRow(table.rows.length);
+                            cell1 = row.insertCell(0);
+                            cell2 = row.insertCell(1);
+                        
+                            cell1.innerHTML = "Number of budgeted items";
+                            cell1.setAttribute("class", "formDesc");
+                            cell2.appendChild(splitCounter);
+                            //document.getElementById("purchaseSplit" + i).appendChild(splitCounter);
+                            document.getElementById("purchaseSplit" + i).hidden = false;
+                        }
+                        else{
+                            table.deleteRow(table.rows.length -1);
+                            var div = document.getElementById("purchaseSplit" + i);
+                            while (div.firstChild) {
+                                div.removeChild(div.firstChild);
+                            }
+                            //document.getElementById("purchaseSplit" + i).innerhtml = "";
+                            div.hidden = true;
+                        }
+                    }
+                    
+                    function splitInputChange(){
+                        var i = this.id.slice(-1);
+                        var splitVal = parseInt(document.getElementById(this.id).value);
+                        if (splitVal < this.dataset.lastValue){ //remove split
+                            index = parseInt(this.dataset.lastValue);
+                            while (index >parseInt(splitVal)){
+                                deleteSplits(index);
+                                index--;
+                            }
+                        }
+                        else{ //add split
+                            var index = parseInt(this.dataset.lastValue)+1;
+                            while(index <=splitVal){
+                                addSplits(i, index);
+                                index++;
+                            }
+                        }
+                        this.dataset.lastValue = splitVal;
+                    }
+                    function addSplits(divID, i) {
+                        var budgets = [];
+                        <?php
+                            for ($i=0; $i<count($budgets); $i++){
+                                echo "budgets[{$i}] = document.createElement(\"option\");";
+                                echo "budgets[{$i}].text = \"" . $budgets[$i] . "\";\n\t\t\t\t\t\t";
+                            }
+                        ?>
+                        var fieldset = document.createElement ("fieldset");
+                        var legend = document.createElement ("legend");
+                        legend.innerHTML = "Budget Split "+ i;
+                        fieldset.appendChild (legend);
+                        fieldset.setAttribute("id", "split" + i);
+                        
+                        var input = [];
+                        input[0] = document.createElement("input");
+                        input[0].type = "number";
+                        input[0].setAttribute("name","splitAmount"+divID+"_" + i);
+                        input[0].setAttribute("min", "0.01");
+                        input[0].setAttribute("step","0.01");
+                        input[0].setAttribute("required","required");
+                        
+                        input[1] = document.createElement("select");
+                        input[1].setAttribute("name","splitBudget" + divID + "_" + i);
+                        input[1].setAttribute("required","required");
+                        for(var index2 = 0; index2< budgets.length; index2++){
+                            input[1].add(budgets[index2]);
+                        }
+                        
+                        var table = document.createElement("table");
+                        table.setAttribute("class", "dataEntry");
+                        var row, cell1, cell2;
+                        var desc = ["Amount: ", "Budget: "];
+                        for(var index2 = 0; index2 <input.length; index2++){
+                            row = table.insertRow(index2);
+                            cell1 = row.insertCell(0);
+                            cell2 = row.insertCell(1);
+                        
+                            cell1.innerHTML = desc[index2];
+                            cell1.setAttribute("class", "formDesc");
+                            cell2.appendChild(input[index2]);
+                        }   
+                        fieldset.appendChild(table); 
+                        document.getElementById("purchaseSplit" + divID).appendChild(fieldset);
+                    }
+                    function deleteSplits(i){
+                        var id = "#split" + i;
+                        $(id).remove();
+                    }
+                    
+                    function validateForm(){ //neds tested
+                        //This function validates the input on the form
+                        //returns true when ok to send to the server
+                        var purCount = parseInt(document.getElementById("numItems").value);
+                        var valid = true;
+                        var budget = "";
+                        var amount, splitAmount;
+                        var splitCount = 0;
+                        for(var i = 1; i<=purcount; i++){
+                            budget = document.forms["mainForm"]["budget"+i].value;
+                            if (budget === "SPLIT"){
+                                amount = document.forms["mainForm"]["amount"+i].value;
+                                //sum up the split budgets
+                                splitAmount = 0;
+                                for(int j=1; j<=parseInt(document.getElementById("splitCounter"+i).value); j++){
+                                    splitAmount += parseFloat(document.forms["mainForm"]["splitAmount" + i + "_" + j].value);
+                                    
+                                }
+                                if(splitAmount != amount){
+                                    valid =false;
+                                    break;
+                                }
+                            } 
+                        }
+                        return valid;
                     }
                     
                     $(document).ready(function(){
